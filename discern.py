9fa7427e6e75c79ea3aab9a25f00d021763f9f83
inji-hanbin <hanbin@jinji-inc.com>
2018-05-03 17:56:18 +0800

    init project

---
+++ discern.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+
+import os
+import pytesseract
+from PIL import Image
+
+def binarizing(img, threshold):
+    """传入image对象进行灰度、二值处理"""
+    img = img.convert("L")  # 转灰度
+    pixdata = img.load()
+    w, h = img.size
+    # 遍历所有像素，大于阈值的为黑色
+    for y in range(h):
+        for x in range(w):
+            if pixdata[x, y] < threshold:
+                pixdata[x, y] = 0
+            else:
+                pixdata[x, y] = 255
+    return img
+
+
+def depoint(img):
+    """传入二值化后的图片进行降噪"""
+    pixdata = img.load()
+    w, h = img.size
+    for y in range(1, h - 1):
+        for x in range(1, w - 1):
+            count = 0
+            if pixdata[x, y - 1] > 245:  # 上
+                count = count + 1
+            if pixdata[x, y + 1] > 245:  # 下
+                count = count + 1
+            if pixdata[x - 1, y] > 245:  # 左
+                count = count + 1
+            if pixdata[x + 1, y] > 245:  # 右
+                count = count + 1
+            if pixdata[x - 1, y - 1] > 245:  # 左上
+                count = count + 1
+            if pixdata[x - 1, y + 1] > 245:  # 左下
+                count = count + 1
+            if pixdata[x + 1, y - 1] > 245:  # 右上
+                count = count + 1
+            if pixdata[x + 1, y + 1] > 245:  # 右下
+                count = count + 1
+            if count > 5:
+                pixdata[x, y] = 255
+    return img
+
+if __name__ == '__main__':
+    im = Image.open('{}/picture/{}'.format(os.getcwd(),'1525339307443406.png'))
+    im.show()
+    binaryImage = binarizing(im, 150)
+    # 降噪
+    binaryImage = depoint(binaryImage)
+    # 展示
+    binaryImage.show()
+    # 转换
+    text = pytesseract.image_to_string(binaryImage, config='-psm 7')
+    print(text)
---
+++ download.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+import os
+import requests
+import time
+
+def downloads_pic(pic_name):
+    url = 'http://cccj.pku.edu.cn/getImgCode.do'
+    res = requests.get(url, stream=True)
+
+    print(res)
+
+    with open('{}/picture/{}'.format(os.getcwd(),pic_name), 'wb') as f:
+        for chunk in res.iter_content(chunk_size=1024):
+            if chunk:  # 过滤下保持活跃的新块
+                f.write(chunk)
+
+                f.flush()
+
+        f.close()
+
+
+if __name__ == '__main__':
+    for i in range(100):
+        pic_name = int(time.time() * 1000000)
+        downloads_pic('{}.{}'.format(pic_name,'png'))
